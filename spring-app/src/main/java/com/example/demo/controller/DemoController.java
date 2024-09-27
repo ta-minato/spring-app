70f4ef29f44d8210410b8da7ec7eb6ca50aebb06
kaitok <kaitokinjo@gmail.com>
2024-09-27 23:49:13 +0900

    add Thymeleaf

--- spring-app/src/main/java/com/example/demo/controller/DemoController.java
+++ spring-app/src/main/java/com/example/demo/controller/DemoController.java
@@ -1,13 +1,16 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DemoController {
-	@GetMapping("/demo")
-	private String display() {
-		return "/index.html";
-	}
+	
+    @GetMapping("/demo")
+    public String hello(Model model) {
+        model.addAttribute("message", "Hello Thymeleaf!!");
+        return "index";
+    }
 
 }
